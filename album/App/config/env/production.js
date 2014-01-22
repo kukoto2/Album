5abed3724ddf19ca208876b6cd0e5723d22e61eb
idito <idi4@abv.bg>
2014-01-22 21:09:26 +0200

    Initial commit.

---
+++ album/App/config/env/production.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+    db: "mongodb://meannet:N_qGE_PC_ybkvzFC54ASImCepIVNIPp_wDKnoWjwI0E-@ds030817.mongolab.com:30817/meannet",
+    app: {
+        name: "MEAN - A Modern Stack - Production"
+    },
+    facebook: {
+        clientID: "APP_ID",
+        clientSecret: "APP_SECRET",
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
+    },
+    twitter: {
+        clientID: "CONSUMER_KEY",
+        clientSecret: "CONSUMER_SECRET",
+        callbackURL: "http://localhost:3000/auth/twitter/callback"
+    },
+    github: {
+        clientID: "APP_ID",
+        clientSecret: "APP_SECRET",
+        callbackURL: "http://localhost:3000/auth/github/callback"
+    },
+    google: {
+        clientID: "APP_ID",
+        clientSecret: "APP_SECRET",
+        callbackURL: "http://localhost:3000/auth/google/callback"
+    }
+}
